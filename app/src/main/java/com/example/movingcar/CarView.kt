6bc26748931ec211d6159220906104cbda3d990f
shindje <aleks.shindje@gmail.com>
2021-06-29 11:20:25 +0300

    Click to start/stop

--- app/src/main/java/com/example/movingcar/CarView.kt
+++ app/src/main/java/com/example/movingcar/CarView.kt
@@ -3,27 +3,38 @@ package com.example.movingcar
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
 
 private const val MARGIN = 100
 
 class CarView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private var isRunning = true
+    private var isRunning = false
     private var isVerticaMove = false   //Вертикальное направление движения
     private var prevVerticaMove = false
-    private var cx = MARGIN
-    private var cy = MARGIN
+    private var cx = 0
+    private var cy = 50
     private val d = 5
     //Дополнительные Bitmap и Canvas для поворота рисунка без поворота всей View
     private var bmp: Bitmap? = null
     private var cnv: Canvas? = null
+    private var textPaint: Paint
+
+    init {
+        textPaint = Paint()
+        textPaint.textSize = 50f
+    }
 
     override fun onDraw(canvas: Canvas?) {
-        if (bmp == null || cnv == null)
+        if (bmp == null || cnv == null) {
             makeBitmapAndCanvas(0)
+        }
         if (canvas != null) {
+            canvas.drawText("Click anywhere to start/stop", 0f, MARGIN/2.toFloat(), textPaint)
             if (isRunning) {
                 prevVerticaMove = isVerticaMove
                 //В конце движемся прямо, иначе - рандом
@@ -61,5 +72,25 @@ class CarView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         cnv = Canvas(bmp!!)
         cnv!!.rotate(angle.toFloat(), (MARGIN/2).toFloat(), (MARGIN/2).toFloat())
         icon.draw(cnv!!)
+    }
+
+    private val myListener =  object : GestureDetector.SimpleOnGestureListener() {
+        override fun onDown(e: MotionEvent): Boolean {
+            return true
+        }
+    }
+
+    private val detector: GestureDetector = GestureDetector(context, myListener)
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return detector.onTouchEvent(event).let { result ->
+            if (!result) {
+                if (event.action == MotionEvent.ACTION_UP) {
+                    isRunning = !isRunning
+                    invalidate()
+                    true
+                } else false
+            } else true
+        }
     }
 }
